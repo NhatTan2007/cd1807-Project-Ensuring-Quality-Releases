9bad465afed0115ebdddb40734381ee3e3f95ab4
TanTTN <nhattan2007@gmail.com>
2023-12-11 21:30:18 +0700

    Update selenium test

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,17 +2,58 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
+def currentTime ()
+    today = date.today()
+    print("Today's date:", today)
 
 # Start the browser and login with standard_user
 def login (user, password):
     print ('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
+    #login test
+    driver.find_element_by_css_selector("input#user-name").send_keys(user)
+    driver.find_element_by_css_selector("input#password").send_keys(password)
+    driver.find_element_by_css_selector("input#login-button").click()
+    print (f'Login with user name {user} and password {password} successfuly')
+    return driver
 
-login('standard_user', 'secret_sauce')
+def add_item_to_cart(driver, itemNumber)
+    print ('Adding items to cart')
+
+    itemElements = driver.find_elements_by_css_selector("div.inventory_item")
+    for element in range(itemElements):
+        descriptionItem = element.find_element_by_css_selector("div.inventory_item_name").text
+        addButtonElement = element.find_element_by_css_selector("button[id^='add-to']").click()
+        print (f'{descriptionItem} was added to cart')
+
+    print (f'Total items was added {itemNumber}')
+
+def remove_item_from_cart(driver, itemNumber)
+    print ('Removing items from cart')
+    itemElements = driver.find_elements_by_css_selector("div.cart_item")
+    itemRemoved = 0
+    for element in range(itemElements):
+        descriptionItem = element.find_element_by_css_selector("div.inventory_item_name").text
+        addButtonElement = element.find_element_by_css_selector("button[id^='remove-']").click()
+        itemRemoved += 1
+        print (f'{descriptionItem} was removed from cart')
+        if(itemRemoved > itemNumber)
+            break
+    
+    print (f'Total items was removed {itemNumber}')
+
+if __name__ == "__main__": 
+    itemNumber = 6 
+    testUsername = 'standard_user' 
+    testPassword = 'secret_sauce'
+    currentTime()
+    driver = login(testUsername, testPassword) 
+    add_item_to_cart(driver, itemNumber)
+    remove_item_from_cart(driver, itemNumber)
+    print(timestamp() + 'Selenium tests are all successfully completed!') 
 
