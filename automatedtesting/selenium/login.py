bc487b55cd2fb5fe46c5b62ea0a45ec04b66b4dc
TanTTN <nhattan2007@gmail.com>
2023-12-13 00:20:16 +0700

    Update python file

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -19,30 +19,30 @@ def login (user, password):
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     #login test
-    driver.find_element_by_css_selector("input#user-name").send_keys(user)
-    driver.find_element_by_css_selector("input#password").send_keys(password)
-    driver.find_element_by_css_selector("input#login-button").click()
+    driver.find_element("css selector", "input#user-name").send_keys(user)
+    driver.find_element("css selector", "input#password").send_keys(password)
+    driver.find_element("css selector", "input#login-button").click()
     print (f'Login with user name {user} and password {password} successfuly')
     return driver
 
 def add_item_to_cart(driver, itemNumber):
     print ('Adding items to cart')
 
-    itemElements = driver.find_elements_by_css_selector("div.inventory_item")
+    itemElements = driver.find_elements("css selector", "div.inventory_item")
     for element in range(itemElements):
-        descriptionItem = element.find_element_by_css_selector("div.inventory_item_name").text
-        addButtonElement = element.find_element_by_css_selector("button[id^='add-to']").click()
+        descriptionItem = element.find_element("css selector", "div.inventory_item_name").text
+        addButtonElement = element.find_element("css selector", "button[id^='add-to']").click()
         print (f'{descriptionItem} was added to cart')
 
     print (f'Total items was added {itemNumber}')
 
 def remove_item_from_cart(driver, itemNumber):
     print ('Removing items from cart')
-    itemElements = driver.find_elements_by_css_selector("div.cart_item")
+    itemElements = driver.find_elements("css selector", "div.cart_item")
     itemRemoved = 0
     for element in range(itemElements):
-        descriptionItem = element.find_element_by_css_selector("div.inventory_item_name").text
-        addButtonElement = element.find_element_by_css_selector("button[id^='remove-']").click()
+        descriptionItem = element.find_element("css selector", "div.inventory_item_name").text
+        addButtonElement = element.find_element("css selector", "button[id^='remove-']").click()
         itemRemoved += 1
         print (f'{descriptionItem} was removed from cart')
         if(itemRemoved > itemNumber):
